9cd18f27f85192999a5a3ea79684cc42944539d5
proXsysSC <147527177+proXsysSC@users.noreply.github.com>
2026-01-29 14:24:18 +0100

    Ajoutez des fichiers projet.

---
+++ ViewModels/BookViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProDataGridDetailRowSample.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public List<AuthorViewModel> Authors {get;set;} = [];
+        public int InStock {  get; set; }
+    }
+
+    public class AuthorViewModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace ProDataGridDetailRowSample.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public List<BookViewModel> Books { get; set; } =
+        [
+            new()
+            {
+                Title = "Astérix le gaulois",
+                Authors =
+                    [
+                        new AuthorViewModel() { Name = "René Goscinny"},
+                        new AuthorViewModel() {Name = "Albert Uderzo"}
+                ],
+                InStock = 100
+
+            },
+            new()
+            {
+                Title = "Spiderman",
+                Authors =
+                [
+                    new AuthorViewModel() {Name = "Stan Lee"},
+                    new AuthorViewModel() {Name = "Steve Ditko"}
+                ],
+                InStock = 15
+            },
+            new()
+            {
+                Title = "Superman",
+                Authors =
+                [
+                    new(){Name = "Jerry Siegel"},
+                    new(){Name = "Joe Shuster"}
+                ],
+                InStock = 1000
+            }
+        ];
+        
+        
+    }
+        
+
+}
